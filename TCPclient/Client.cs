c6b4e70ebd9029edb7fed6d1dab3d1a1f24bab82
Piotr Uhl <piotruhl@gmail.com>
2020-08-25 11:56:10 +0200

    Implemented primitive Caesar cipher encoder

--- TCPclient/Client.cs
+++ TCPclient/Client.cs
@@ -1,12 +1,51 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
 
-namespace TCPclient
-{
-    class Client
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace TCPclient {
+    class Client {
+        public static void Main() {
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            UInt16 port = 50000;
+            TcpClient tcpClient = null;
+            try {
+                while (true)
+                {
+                    Console.Write("Enter data or \'exit\': ");
+                    String outMessage = Console.ReadLine();
+                    if (outMessage.Equals("exit"))
+                        break;
+
+                    Console.WriteLine("Connecting...");
+                    tcpClient = new TcpClient();
+                    tcpClient.Connect(ip, port);
+                    Console.WriteLine("Connected");
+
+                    Stream stream = tcpClient.GetStream(); //get stream from connection
+
+                    byte[] outData = System.Text.Encoding.ASCII.GetBytes(outMessage); //convert string to raw data
+                    stream.Write(outData, 0, outData.Length); //write data to stream
+                    Console.WriteLine("Sent message:     \"" + outMessage + '\"');
+
+                    byte[] inData = new byte[256];
+                    int bytes = stream.Read(inData, 0, 256); //read incomming data from stream
+                    String inMessage = System.Text.Encoding.ASCII.GetString(inData, 0, bytes);
+                    Console.WriteLine("Recieved message: \"" + inMessage + '\"');
+
+                    stream.Close();
+                    tcpClient.Close();
+                    tcpClient = null;
+                }
+            }
+            catch (Exception e) {
+                Console.WriteLine("Error!: " + e.StackTrace);
+            }
+            finally {
+                if (tcpClient != null && tcpClient.Connected)
+                    tcpClient.Close();
+            }
         }
     }
 }
--- TCPserver/Server.cs
+++ TCPserver/Server.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
 
-namespace TCPserver
-{
-    class Server
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace TCPserver {
+    class Server {
+        public static void Main() {
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            UInt16 port = 50000;
+            TcpListener listener = null;
+            Socket socket = null;
+            try {
+                listener = new TcpListener(ip, port);
+                listener.Start();
+                Console.WriteLine("Listening at " + port);
+                while (true)
+                {
+                    socket = listener.AcceptSocket(); //accept connection
+                    Console.WriteLine("Connection accepted from " + socket.RemoteEndPoint);
+
+                    byte[] data = new byte[256];
+                    int bytes = socket.Receive(data); //get data
+                    String message = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                    Console.WriteLine("Recieved message: \"" + message + '\"');
+                    String newMessage = String.Empty;
+                    foreach (var k in message) { //encode data with Caesar cipher
+                        newMessage += Convert.ToChar(Convert.ToUInt16(k) + 5);
+					}
+                    socket.Send(System.Text.Encoding.ASCII.GetBytes(newMessage)); //send encoded data
+                    Console.WriteLine("Sent message: \"" + newMessage + '\"');
+
+                    socket.Close(); //close connection
+                    socket = null;
+                }
+            }
+            catch (Exception e) {
+                Console.WriteLine("Error!: " + e.StackTrace);
+            }
+            finally {
+                if (socket != null)
+                    socket.Close();
+                if (listener != null)
+                    listener.Stop();
+            }
         }
     }
 }
