037ba67bde8b61a2dd146773e6c56291e00e1410
Piotr Uhl <piotruhl@gmail.com>
2020-08-25 10:55:28 +0200

    Created solution and empty projects

---
+++ TCPserver/Server.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TCPserver
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
